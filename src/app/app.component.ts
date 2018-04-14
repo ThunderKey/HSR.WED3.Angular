2c5be45eac0649b3d363738b4628c477bee2bfa6
Nicolas Ganz <nicolas@keltec.ch>
2018-04-14 12:56:21 +0200

    improved the navigation and added a logout button
    Closes #5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
 import {Component, OnInit} from '@angular/core';
 
+import {NavigationService} from './core/services';
+
+import {AuthService} from './auth/services';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  constructor(private autSvc: AuthService, private navigationSvc: NavigationService) {
+  }
+
   ngOnInit() {
     $(document).foundation();
+  }
+
+  doLogout() {
+    this.autSvc.logout();
+    this.navigationSvc.goToHome();
   }
 }
